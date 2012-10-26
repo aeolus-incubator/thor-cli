e8c801fdafab7cb0265f3a87df88750a7f8f0785
Crag Wolfe <cwolfe@redhat.com>
2012-10-26 12:53:10 -0400

    Thor scaffolding and "add provider" works against conductor API.

---
+++ lib/aeocli/version.rb
@@ -0,0 +1,3 @@
+module Aeocli
+  VERSION = "0.0.1"
+end
---
+++ spec/provider_spec.rb
@@ -0,0 +1,6 @@
+require 'aeocli'
+require 'aeocli/provider'
+#
+# See cucumber tests for testing command line functionality
+# Non-command line tests wil go here
+#
