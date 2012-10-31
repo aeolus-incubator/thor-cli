98149e26bc971130bf80f4928c025d4354798efc
Crag Wolfe <cwolfe@redhat.com>
2012-10-31 04:00:35 -0400

    Add custom xml formatters to our classes that inherit from ActiveResource
    so we don't have to override ActiveResource's instantiate_collection method.

--- lib/aeolus_cli/model/provider_type.rb
+++ lib/aeolus_cli/model/provider_type.rb
@@ -1,2 +1,12 @@
+class ProviderTypeXMLFormat
+  include ActiveResource::Formats::XmlFormat
+
+  def decode(xml)
+    puts "xml is "+xml
+    ActiveResource::Formats::XmlFormat.decode(xml)['provider_type']
+  end
+end
+
 class AeolusCli::Model::ProviderType < AeolusCli::Model::Base
+  self.format = ProviderTypeXMLFormat.new
 end
