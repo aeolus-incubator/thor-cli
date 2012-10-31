07d4e0266db236d6057a42d1e8988c0c07eeda4c
Crag Wolfe <cwolfe@redhat.com>
2012-10-31 04:10:36 -0400

    Oops... removing a puts.

--- lib/aeolus_cli/model/provider_type.rb
+++ lib/aeolus_cli/model/provider_type.rb
@@ -2,7 +2,6 @@ class ProviderTypeXMLFormat
   include ActiveResource::Formats::XmlFormat
 
   def decode(xml)
-    puts "xml is "+xml
     ActiveResource::Formats::XmlFormat.decode(xml)['provider_type']
   end
 end
